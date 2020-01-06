9864a6862899a03c67736cd2b79f85e625bb37f4
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2020-01-06 20:21:30 +0100

    add implementation

---
+++ Logger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace JFuerlinger.Utils
+{
+    public class Logger
+    {
+        public static void Log(string message) {
+            Console.WriteLine(message);
+        }
+    }
+}
